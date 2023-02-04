718cf4da4f1696f7182c115fe54519cd0c5b59d8
Ryan <ryan.dinh@ontariotechu.net>
2023-02-03 22:28:49 -0500

    Water meter added

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Water : MonoBehaviour
+{
+    public Slider waterMeter;
+    float maxWater = 10;
+    float currentWater;
+    public static Water instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        currentWater = maxWater;
+        waterMeter.maxValue = maxWater;
+        waterMeter.value = maxWater;
+    }
+
+    //Deplete the water meter
+    void Update()
+    {
+        currentWater -= Time.deltaTime;
+
+        if (currentWater > 0)
+        {
+            waterMeter.value = currentWater;
+        }
+        else
+        {
+            Debug.Log("Dehydrated");
+        }
+    }
+
+    //For when you collide with a pocket of water
+    public void WaterAddition(int amount)
+    {
+        if (currentWater + amount <= 10)
+        {
+            currentWater += amount;
+            waterMeter.value = currentWater;
+        }
+        else
+        {
+            Debug.Log("Tree died");
+        }
+    }
+
+}
